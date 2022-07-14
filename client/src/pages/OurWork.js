964002693184f2be866661678d030637d8bbec45
ryuhk <yhklilly0305@gmail.com>
2022-07-14 19:03:04 +0900

    aos delay changed

--- client/src/pages/OurWork.js
+++ client/src/pages/OurWork.js
@@ -59,12 +59,12 @@ const OurWork = ({sections}) => {
           
           <ul>
             {
-              ourWorks.slice(0, endPoint).map((work) => {
+              ourWorks.slice(0, endPoint).map((work, index) => {
                 return (
                   <li 
                     key={work.id}
                     data-aos="fade-up"
-                    data-aos-delay={`${work.id * 2}00`}
+                    data-aos-delay={`${(index % 3) * 2}00`}
                     onClick={
                       () => {
                         setWorkIndex(work.id - 1)
@@ -76,7 +76,7 @@ const OurWork = ({sections}) => {
                       <span><em>DETAIL</em></span>
                     </div>
                     <figure>
-                      <img src={work.image[0]} alt={`${work.titile} thumbnail`}/>
+                      <img src={work.image[0]} alt={`${work.title} thumbnail`}/>
                     </figure>
                   </li>
                 )
