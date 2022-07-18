5e2c711f3837896b1161d186329ac6eafd8a30bc
ryuhk <yhklilly0305@gmail.com>
2022-07-18 16:04:03 +0900

    map styling

--- client/src/components/contact-form/Map.js
+++ client/src/components/contact-form/Map.js
@@ -18,6 +18,7 @@ const Map = () => {
           icon={anchor}
           position={{ lat: 37.492708, lng: 127.015470 }}
           animation={2}
+          title="서울특별시 서초구 서초중앙로 20길 33-17, 서초빌리지 1, 202호"
           // onClick={() => {
           //   alert('hello');
           // }}
