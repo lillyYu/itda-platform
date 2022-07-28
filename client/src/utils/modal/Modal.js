659cbd1f8855cab7f485d16bc72c7135a2bd5055
ryuhk <yhklilly0305@gmail.com>
2022-07-28 17:58:59 +0900

    modal 클릭시 외부영역 스크롤 방지

--- client/src/utils/modal/Modal.js
+++ client/src/utils/modal/Modal.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 
 export default function Modal ({ 
   show, 
@@ -5,6 +6,22 @@ export default function Modal ({
   children,
   setImgIndex
 }) {
+  
+  useEffect(() => {
+    if(show) {
+      document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+      return () => {
+        const scrollY = document.body.style.top;
+        document.body.style.cssText = '';
+        window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+      };
+    }
+  }, [show]);
+
   return (
     <div className={show ? "modalWrap active" : "modalWrap"}>
       <div
