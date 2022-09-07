9ca740b4c19a69754aeacbfb91d5fc7bf10291d2
lilly0305 <yhklilly0305@gmail.com>
2022-09-07 11:40:29 +0900

    commit after form validation error solved

--- client/src/components/contact-form/ContactForm.js
+++ client/src/components/contact-form/ContactForm.js
@@ -4,49 +4,57 @@ import Captcha from "components/elements/Captcha";
 import { useContext, useEffect, useState } from "react";
 import LoadingSpinner from "components/elements/LoadingSpinner";
 import AlertMessage from "components/elements/AlertMessage";
-import ModalPortal from 'utils/modal/ModalPortal';
+import ModalPortal from "utils/modal/ModalPortal";
 import CodeTitle from "components/elements/CodeTitle";
 import GeneralContext from "utils/context/GeneralContext";
 import { FormattedMessage } from "react-intl";
 
 const ContactForm = () => {
-  const {language} = useContext(GeneralContext);
-  
+  const { language } = useContext(GeneralContext);
+
   const [message, setMessage] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const { register, trigger, handleSubmit, reset, watch, formState: { errors } } = useForm( { mode: "onBlur",
+  const {
+    register,
+    trigger,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
     defaultValues: {
       name: "",
       phone: "",
       email: "",
       title: "",
-      message: ""
-    }
+      message: "",
+    },
   });
 
   const submitHandler = async (data) => {
-    setLoading(true)
+    setLoading(true);
 
     try {
-      const res = await axios.post('/api/v1/mail/mail-send', {
+      const res = await axios.post("/api/v1/mail/mail-send", {
         name: data.name,
         phone: data.phone,
         email: data.email,
         title: data.title,
-        message: data.message
-      })
+        message: data.message,
+      });
 
-      if(res.status === 200){
+      if (res.status === 200) {
         setLoading(false);
         setMessage(true);
         reset();
       }
     } catch (error) {
-      setLoading(false)
-      console.log(error)
+      setLoading(false);
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
     if (message) {
@@ -54,132 +62,168 @@ const ContactForm = () => {
     }
   }, [message]);
 
-  return (
-    loading 
-    ? <div className="alignCenter"><LoadingSpinner /></div>
-    :
+  return loading ? (
+    <div className="alignCenter">
+      <LoadingSpinner />
+    </div>
+  ) : (
     <>
       <form onSubmit={handleSubmit(submitHandler)} data-aos="fade-left">
-        <CodeTitle title="ask to ITDA"/>
+        <CodeTitle title="ask to ITDA" />
 
         <div className="form-wrap">
           <div className="inputLabel">
-            <input 
-              type="text" 
+            <input
+              type="text"
               id="name"
               className={
-                watch('name') !== "" ? "active" : errors.name ? "errorBorder" : ""
+                watch("name") !== ""
+                  ? "active"
+                  : errors.name
+                  ? "errorBorder"
+                  : ""
               }
               autoComplete="off"
               {...register("name", { required: true })}
             />
             <label htmlFor="name">name</label>
-            {errors.name && <span className="error">
-              <FormattedMessage
-                id="field.required"
-                defaultMessage="*필수 입력 항목입니다."
-              />
-            </span>}
+            {errors.name && (
+              <span className="error">
+                <FormattedMessage
+                  id="field.required"
+                  defaultMessage="*필수 입력 항목입니다."
+                />
+              </span>
+            )}
           </div>
-  
+
           <div className="inputLabel">
-            <input 
-              type="phone" 
+            <input
+              type="phone"
               id="phone"
               className={
-                watch('phone') !== "" ? "active" : errors.phone ? "errorBorder" : ""
+                watch("phone") !== ""
+                  ? "active"
+                  : errors.phone
+                  ? "errorBorder"
+                  : ""
               }
               autoComplete="off"
               {...register("phone", {
-                required: language === "ko" ? "*필수 입력 항목입니다." : "This field is required.", 
-                pattern:{
-                  value: /^[0-9]+$/, 
-                  message: language === "ko" ? "숫자만 입력해주세요." : "Please enter a number"}})
-                } 
+                required:
+                  language === "ko"
+                    ? "*필수 입력 항목입니다."
+                    : "This field is required.",
+                pattern: {
+                  value: /^[0-9]+$/,
+                  message:
+                    language === "ko"
+                      ? "숫자만 입력해주세요."
+                      : "Please enter a number",
+                },
+              })}
             />
             <label htmlFor="phone">phone</label>
-            {errors.phone && <span className="error">{errors.phone.message}</span>}
+            {errors.phone && (
+              <span className="error">{errors.phone.message}</span>
+            )}
           </div>
-  
+
           <div className="inputLabel">
-            <input 
-              type="text" 
+            <input
+              type="text"
               id="email"
               className={
-                watch('email') !== "" ? "active" : errors.email ? "errorBorder" : ""
+                watch("email") !== ""
+                  ? "active"
+                  : errors.email
+                  ? "errorBorder"
+                  : ""
               }
               autoComplete="off"
               {...register("email", {
-                required: language === "ko" ? "*필수 입력 항목입니다." : "This field is required.", 
-                pattern:{
-                  value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i, 
-                  message: language === "ko" ? "이메일 형식이 아닙니다." : "Please check email format."}})
-                } 
+                required:
+                  language === "ko"
+                    ? "*필수 입력 항목입니다."
+                    : "This field is required.",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i,
+                  message:
+                    language === "ko"
+                      ? "이메일 형식이 아닙니다."
+                      : "Please check email format.",
+                },
+              })}
             />
             <label htmlFor="email">email</label>
-            {errors.email && <span className="error">{errors.email.message}</span>}
+            {errors.email && (
+              <span className="error">{errors.email.message}</span>
+            )}
           </div>
-  
+
           <div className="inputLabel">
-            <input 
-              type="text" 
+            <input
+              type="text"
               id="title"
               className={
-                watch('title') !== "" ? "active" : errors.title ? "errorBorder" : ""
+                watch("title") !== ""
+                  ? "active"
+                  : errors.title
+                  ? "errorBorder"
+                  : ""
               }
               autoComplete="off"
               {...register("title", { required: true })}
             />
             <label htmlFor="title">title</label>
-            {errors.title && <span className="error">
-              <FormattedMessage
-                id="field.required"
-                defaultMessage="*필수 입력 항목입니다."
-              />
-            </span>}
+            {errors.title && (
+              <span className="error">
+                <FormattedMessage
+                  id="field.required"
+                  defaultMessage="*필수 입력 항목입니다."
+                />
+              </span>
+            )}
           </div>
-  
+
           <div className="inputLabel">
-            <textarea 
+            <textarea
               id="message"
-              className={
-                watch('message') !== "" ? "active" : ""
-              }
+              className={watch("message") !== "" ? "active" : ""}
               {...register("message")}
             />
             <label htmlFor="message">other message</label>
           </div>
         </div>
-  
-        <Captcha 
+
+        <Captcha
           register={register}
           errors={errors}
           trigger={trigger}
           watch={watch}
         />
-  
+
         <div className="alignCenter">
           <button type="submit">SUBMIT</button>
         </div>
       </form>
       <ModalPortal>
         <AlertMessage show={message}>
-          {
-            language === "ko"
-            ? <p>
-                문의 메일이 정상적으로 발송되었습니다.
-                <br/>곧 연락드리겠습니다!
-              </p>
-            : <p>
-                Contact form has successfully submitted.
-                <br/> We will contact you soon!
-              </p>
-          }
-          
+          {language === "ko" ? (
+            <p>
+              문의 메일이 정상적으로 발송되었습니다.
+              <br />곧 연락드리겠습니다!
+            </p>
+          ) : (
+            <p>
+              Contact form has successfully submitted.
+              <br /> We will contact you soon!
+            </p>
+          )}
         </AlertMessage>
       </ModalPortal>
     </>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
--- client/src/components/elements/Captcha.js
+++ client/src/components/elements/Captcha.js
@@ -3,22 +3,21 @@ import GeneralContext from "utils/context/GeneralContext";
 
 let code;
 
-const Captcha = ({register, errors, trigger, watch}) => {
-  const {language} = useContext(GeneralContext)
+const Captcha = ({ register, errors, trigger, watch }) => {
+  const { language } = useContext(GeneralContext);
   const captchaImg = useRef();
 
   const createCaptcha = () => {
     captchaImg.current.innerHTML = "";
-    const charsArray =
-    "0123456789abcdefghijklmnopqrstuvwxyz";
+    const charsArray = "0123456789abcdefghijklmnopqrstuvwxyz";
     const lengthOtp = 6;
     const captcha = [];
 
     for (var i = 0; i < lengthOtp; i++) {
-        const index = Math.floor(Math.random() * charsArray.length + 1); 
-        if (captcha.indexOf(charsArray[index]) === -1)
+      const index = Math.floor(Math.random() * charsArray.length + 1);
+      if (captcha.indexOf(charsArray[index]) === -1)
         captcha.push(charsArray[index]);
-        else i--;
+      else i--;
     }
 
     const canv = document.createElement("canvas");
@@ -33,51 +32,56 @@ const Captcha = ({register, errors, trigger, watch}) => {
     code = captcha.join("");
 
     captchaImg.current.appendChild(canv);
-  }
+  };
 
   useEffect(() => {
-    createCaptcha()
-  }, [])
+    createCaptcha();
+  }, []);
 
   return (
     <div className="captcha-form">
       <div>
-        <div 
-          id="captcha"
-          className="captcha"
-          ref={captchaImg}
-        />
-        
+        <div id="captcha" className="captcha" ref={captchaImg} />
+
         <i
           className="ri-restart-line"
-          onClick={ async () => {
+          onClick={async () => {
             createCaptcha();
-            await trigger("cpatchaText");
+            await trigger("captchaText");
           }}
         />
       </div>
 
       <div className="inputLabel">
-        <input 
-          type="text" 
-          id="cpatchaText"
+        <input
+          type="text"
+          id="captchaText"
           autoComplete="off"
           className={
-            watch('cpatchaText') !== "" ? "active" : errors.cpatchaText ? "errorBorder" : ""
+            watch("captchaText") !== ""
+              ? "active"
+              : errors.captchaText
+              ? "errorBorder"
+              : ""
           }
-          {...register("cpatchaText", {
-            required:"*필수 입력 항목입니다.", 
+          {...register("captchaText", {
+            required: "*필수 입력 항목입니다.",
             validate: {
-              value: (v) => v === code || language === "ko" ? "코드가 일치하지 않습니다." : "Code is not matched"
-            }
-          }
-          )}
+              value: (v) =>
+                v === code ||
+                (language === "ko"
+                  ? "코드가 일치하지 않습니다."
+                  : "Code is not matched"),
+            },
+          })}
         />
-        <label htmlFor="cpatchaText">captcha</label>
-        {errors.cpatchaText && <span className="error">{errors.cpatchaText.message}</span>}
+        <label htmlFor="captchaText">captcha</label>
+        {errors.captchaText && (
+          <span className="error">{errors.captchaText.message}</span>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Captcha
+export default Captcha;
